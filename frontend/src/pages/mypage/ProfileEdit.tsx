0e4cfc36352e8748d9f20d77ca4465b400260cf7
Harrycho415 <harrycho415@gmail.com>
2025-01-27 17:02:55 +0900

    토글버튼 및 마이페이지 프로필 박스 수정

--- frontend/src/pages/mypage/ProfileEdit.tsx
+++ frontend/src/pages/mypage/ProfileEdit.tsx
@@ -4,5 +4,61 @@ import React from 'react';
 export default function ProfileEdit() {
   const router = useRouter();
 
-  return <div className="w-full h-full bg-white text-black rounded-xl"> </div>;
+  return (
+    <div className="w-full h-full bg-white text-black rounded-xl p-[24px]">
+      <div className="mb-[24px]">
+        <p className="mb-[8px]">Name</p>
+        <input
+          type="text"
+          placeholder="Value"
+          className="border border-gray-300 p-2 rounded w-full"
+        />
+      </div>
+      <div className="mb-[24px]">
+        <p className="mb-[8px]">Research Field</p>
+        <input
+          type="text"
+          placeholder="Value"
+          className="border border-gray-300 p-2 rounded w-full"
+        />
+      </div>
+      <div className="mb-[24px]">
+        <p className="mb-[8px]">Email</p>
+        <input
+          type="text"
+          placeholder="Value"
+          className="border border-gray-300 p-2 rounded w-full"
+        />
+      </div>
+      <div className="mb-[24px]">
+        <p className="mb-[8px]">Lab URL</p>
+        <input
+          type="text"
+          placeholder="Value"
+          className="border border-gray-300 p-2 rounded w-full"
+        />
+      </div>
+      <div className="mb-[24px]">
+        <p className="mb-[8px]">Contact Number</p>
+        <input
+          type="text"
+          placeholder="Value"
+          className="border border-gray-300 p-2 rounded w-full"
+        />
+      </div>
+      <div className="mb-[24px]">
+        <p className="mb-[8px]">Lab Description</p>
+        <input
+          type="text"
+          placeholder="Value"
+          className="border border-gray-300 p-2 rounded w-full"
+        />
+      </div>
+      <div>
+        <button className="bg-gray-500 hover:bg-gray-700 text-white py-2 px-4 rounded w-full">
+          Edit
+        </button>
+      </div>
+    </div>
+  );
 }
